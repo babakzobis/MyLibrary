61ab96c3b3369d0683f99de630226e61d7731375
babakzo <github01@vanzoconsulting.com>
2022-09-05 03:14:14 +0200

    Parcelizing the movie model for recovery purposes

--- app/src/main/java/com/vanzoconsulting/mylibrary/domain/entity/Movie.kt
+++ app/src/main/java/com/vanzoconsulting/mylibrary/domain/entity/Movie.kt
@@ -1,23 +1,17 @@
 package com.vanzoconsulting.mylibrary.domain.entity
 
-import com.squareup.moshi.Json
+import android.os.Parcelable
 import com.squareup.moshi.JsonClass
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @JsonClass(generateAdapter = true)
 /**
- * todo: local storage via Room
+ * todo: local storage via Room and sync it with the API
  */
 data class Movie(
-
-    @field:Json(name = "title")
     val title: String? = null,
-
-    @field:Json(name = "year")
     val year: Int? = null,
-
-    @field:Json(name = "director")
     val director: String? = null,
-
-    @field:Json(name = "posterUrl")
     val posterUrl: String? = null
-)
+) : Parcelable
